c3ae15c1707390b10a0753ce08cc6b8382de63c1
Hayden Braxton <haydenbraxton@unboxedtechnology.com>
2018-12-02 02:13:29 -0500

    add injector to admin login component

--- src/app/admin/components/admin-login/admin-login.component.ts
+++ src/app/admin/components/admin-login/admin-login.component.ts
@@ -3,27 +3,31 @@ import * as angular from 'angular';
 angular.module('app').component('adminLogin', {
 	templateUrl: './admin-login.component.html',
 	bindings: {},
-	controller: function($location, currentIdentity, auth, toastr) {
-		this.loggedIn = currentIdentity.authenticated();
-
-		if (this.loggedIn) {
-			$location.path('/home');
-		}
-
-		this.login = function() {
-			auth
-				.login({
-					username: this.email,
-					password: this.password
-				})
-				.then(
-					function() {
-						$location.path('/home');
-					},
-					function(err) {
-						toastr.error(err);
-					}
-				);
-		};
-	}
+	controller: adminLoginCtrl
 });
+
+adminLoginCtrl.$inject = ['$location', 'currentIdentity', 'auth', 'toastr']
+
+function adminLoginCtrl($location, currentIdentity, auth, toastr) {
+	this.loggedIn = currentIdentity.authenticated();
+
+	if (this.loggedIn) {
+		$location.path('/home');
+	}
+
+	this.login = function() {
+		auth
+			.login({
+				username: this.email,
+				password: this.password
+			})
+			.then(
+				function() {
+					$location.path('/home');
+				},
+				function(err) {
+					toastr.error(err);
+				}
+			);
+	};
+}
