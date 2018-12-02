b05c5ed982e6d80e977921857010581ad61aeab9
Hayden Braxton <haydenbraxton@unboxedtechnology.com>
2018-12-02 03:19:54 -0500

    convert auth service to class and service instead of function

--- src/app/security/services/auth.ts
+++ src/app/security/services/auth.ts
@@ -1,41 +1,50 @@
 import * as angular from 'angular';
 
-angular.module('app').factory('auth', ($http, currentIdentity) => ({
-	login: (credentials) =>
-		$http
+export class AuthService {
+	static $inject = ['$http', 'currentIdentity'];
+	constructor(private $http, private currentIdentity) {}
+
+	login(credentials) {
+		return this.$http
 			.post('http://localhost:8801/api/login', credentials, { withCredentials: true })
-			.then((response) => currentIdentity.setUser(response.data.user))
+			.then((response) => this.currentIdentity.setUser(response.data.user))
 			.catch(() => {
 				throw 'Invalid Credentials';
-			}),
-	logout: () =>
-		$http
+			})
+	}
+
+	logout() {
+		return this.$http
 			.post('http://localhost:8801/api/logout', {}, { withCredentials: true })
-			.then(() => currentIdentity.clearUser())
+			.then(() => this.currentIdentity.clearUser())
 			.catch(() => {
 				throw 'Error Logging Out';
-			}),
+			});
+	}
 
-	waitForAuth: () =>
-		$http.get('http://localhost:8801/api/currentIdentity', { withCredentials: true }).then((response) => {
+	waitForAuth() {
+		return this.$http.get('http://localhost:8801/api/currentIdentity', { withCredentials: true }).then((response) => {
 			if (!!response.data) {
-				currentIdentity.setUser(response.data);
+				this.currentIdentity.setUser(response.data);
 			}
-			return currentIdentity;
-		}),
+			return this.currentIdentity;
+		})
+	}
 
-	requireLogin: function() {
+	requireLogin() {
 		return this.waitForAuth().then(
-			() => currentIdentity.authenticated() || Promise.reject('AUTH_REQUIRED')
+			() => this.currentIdentity.authenticated() || Promise.reject('AUTH_REQUIRED')
 		);
-	},
+	}
 
-	requireAdmin: function() {
+	requireAdmin() {
 		return this.waitForAuth().then(
 			() =>
-				(currentIdentity.authenticated() &&
-					currentIdentity.currentUser.isAdmin) ||
+				(this.currentIdentity.authenticated() &&
+					this.currentIdentity.currentUser.isAdmin) ||
 				Promise.reject('AUTH_REQUIRED')
 		);
-	},
-}));
+	}
+}
+
+angular.module('app').service('auth', AuthService);
