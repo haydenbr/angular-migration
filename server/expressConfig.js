6b49808090b976512d582c107e0bd8e05fb42818
Hayden Braxton <haydenbraxton@unboxedtechnology.com>
2018-12-02 02:12:02 -0500

    can only have one value for allowed origin header

--- server/expressConfig.js
+++ server/expressConfig.js
@@ -5,7 +5,7 @@ var logger = require('morgan'),
 
 module.exports = function(app) {
 	app.use((req, res, next) => {
-		res.header('Access-Control-Allow-Origin', 'http://localhost:4200, http://localhost:8000');
+		res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
 		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 		res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 		res.header('Access-Control-Allow-Credentials', 'true');
