bd8f1fce3d01fb75902f16904a5b7db6f3f5b5a8
Hayden Braxton <haydenbraxton@unboxedtechnology.com>
2018-12-02 03:30:54 -0500

    components to classes

--- src/app/sessions/components/session-detail-with-votes/session-detail-with-votes.component.ts
+++ src/app/sessions/components/session-detail-with-votes/session-detail-with-votes.component.ts
@@ -1,10 +1,15 @@
 import * as angular from 'angular';
 
+export class SessionDetailWithVotesComponent {
+	session: any;
+	initialCollapsed: any;
+}
+
 angular.module('app').component('sessionDetailWithVotes', {
 	templateUrl: './session-detail-with-votes.component.html',
 	bindings: {
 		session: '=',
 		initialCollapsed: '@',
 	},
-	controller: function() {},
+	controller: SessionDetailWithVotesComponent
 });
--- src/app/sessions/components/session-detail/session-detail.component.ts
+++ src/app/sessions/components/session-detail/session-detail.component.ts
@@ -1,10 +1,15 @@
 import * as angular from 'angular';
 
+export class SessionDetailComponent {
+	session: any;
+	initialCollapsed: any;
+}
+
 angular.module('app').component('sessionDetail', {
 	templateUrl: './session-detail.component.html',
 	bindings: {
 		session: '=',
 		initialCollapsed: '@',
 	},
-	controller: function() {},
+	controller: SessionDetailComponent
 });
