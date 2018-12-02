1b9711be47097f5692636445215bc5599c84784c
Hayden Braxton <haydenbraxton@unboxedtechnology.com>
2018-12-02 03:23:27 -0500

    current identity service not function ... with a class

--- src/app/security/services/currentIdentity.ts
+++ src/app/security/services/currentIdentity.ts
@@ -1,27 +1,34 @@
 import * as angular from 'angular';
 
-angular.module('app').factory('currentIdentity', function($http) {
-	return {
-		currentUser: null,
-		setUser: function(user) {
-			this.currentUser = user;
-		},
-		clearUser: function() {
-			this.currentUser = null;
-		},
-		authenticated: function() {
-			return !!this.currentUser;
-		},
-		updateUser: function(newUserObj) {
-			return $http
-				.put('http://localhost:8801/api/users/' + this.currentUser.id, newUserObj, { withCredentials: true })
-				.then(() => {
-					this.currentUser.firstName = newUserObj.firstName;
-					this.currentUser.lastName = newUserObj.lastName;
-				})
-				.catch(() => {
-					throw 'Error Logging Out';
-				});
-		},
-	};
-});
+export class CurrentIdentity {
+	currentUser;
+
+	static $inject = ['$http'];
+	constructor(private $http) {}
+
+	setUser(user) {
+		this.currentUser = user;
+	}
+
+	clearUser() {
+		this.currentUser = null;
+	}
+
+	authenticated() {
+		return !!this.currentUser;
+	}
+
+	updateUser(newUserObj) {
+		return this.$http
+			.put('http://localhost:8801/api/users/' + this.currentUser.id, newUserObj, { withCredentials: true })
+			.then(() => {
+				this.currentUser.firstName = newUserObj.firstName;
+				this.currentUser.lastName = newUserObj.lastName;
+			})
+			.catch(() => {
+				throw 'Error Logging Out';
+			});
+	}
+}
+
+angular.module('app').service('currentIdentity', CurrentIdentity);
