c55ce04429491a47d25d90ea3738b372d25a8a80
Hayden Braxton <haydenbraxton@unboxedtechnology.com>
2018-12-02 02:39:20 -0500

    static inject user list

--- src/app/admin/components/user-list/user-list.component.ts
+++ src/app/admin/components/user-list/user-list.component.ts
@@ -1,15 +1,21 @@
 import * as angular from 'angular';
 
+class UserListComponent {
+	private allUsers: any[];
+
+	$onInit() {
+		this.allUsers = this.allUsers.concat().sort((user1, user2) => {
+			if (user1.firstName < user2.firstName) return -1;
+			if (user1.firstName === user2.firstName) return 0;
+			if (user1.firstName > user2.firstName) return 1;
+		});
+	}
+}
+
 angular.module('app').component('userList', {
 	templateUrl: './user-list.component.html',
 	bindings: {
 		allUsers: '<'
 	},
-	controller: function() {
-		this.allUsers && this.allUsers.sort((user1, user2) => {
-			if (user1.firstName < user2.firstName) return -1;
-			if (user1.firstName === user2.firstName) return 0;
-			if (user1.firstName > user2.firstName) return 1;
-		});
-	},
+	controller: UserListComponent
 });
