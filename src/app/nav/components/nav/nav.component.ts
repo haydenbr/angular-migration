8f54beb1558e3c466ba65c39ade2fa9051294d38
Hayden Braxton <haydenbraxton@unboxedtechnology.com>
2018-12-02 02:56:27 -0500

    static inject nav module

--- src/app/nav/components/nav/nav.component.ts
+++ src/app/nav/components/nav/nav.component.ts
@@ -1,12 +1,24 @@
 import * as angular from 'angular';
 
+export class NavComponent {
+	currentUser: any;
+
+	static $inject = ['currentIdentity', 'sessions', 'unreviewedSessionCount'];
+	constructor (
+		private currentIdentity,
+		private sessions,
+		private unreviewedSessionCount
+	) {}
+
+	$onInit() {
+		this.currentUser = this.currentIdentity.currentUser;
+		this.unreviewedSessionCount.updateUnreviewedSessionCount();
+	}
+}
+
+
 angular.module('app').component('nav', {
 	templateUrl: './nav.component.html',
 	bindings: {},
-	controller: function(currentIdentity, sessions, unreviewedSessionCount) {
-		this.currentUser = currentIdentity.currentUser;
-
-		unreviewedSessionCount.updateUnreviewedSessionCount();
-		this.unreviewedSessionCount = unreviewedSessionCount;
-	},
+	controller: NavComponent
 });
